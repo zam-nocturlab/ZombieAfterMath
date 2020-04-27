d562f9bf0a49d74c16f50d68c2d8f05fd0aa695e
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-04-27 11:09:31 +0200

    Fix English translations not complete. (#27)

--- schema/languages/sh_english.lua
+++ schema/languages/sh_english.lua
@@ -1,30 +1,106 @@
 LANGUAGE = {
-	reserveSalary = "Your Bank Reserve increased by %s for Salary",
-	reserveIncome = "Your Bank Reserve increased by %s for Bank Income",
-	salaryRejected = "You couldn't receive the salary",
-	bankMenu = "Bank Menu",
-	bankReserve = "Bank Reserve: %s",
+	
+	reserveSalary = "You have just received a letter from your employer: Your salary of %s has just been paid into your account",
+	reserveIncome = "Your bank interest has just been transferred: %s",
+	salaryRejected = "You could not receive a salary",
+	bankMenu = "Bank",
+	bankReserve = "Budgetary reserve: %s",
 	deposit = "Deposit",
 	withdraw = "Withdraw",
 	transfer = "Transfer",
-	depositMoney = "You added %s to your Bank Reserve",
-	withdrawMoney = "You took %s from your Bank Reserve",
-	hospitalPrice = "Your Bank Reserve deducted by %s for the Medical Care",
-	provideValidNumber = "You provided wrong number on amount argument.",
-	cantAfford = "You can't afford this",
-	enterAmount = "Enter Amount",
-	requireAttrib = "You have to train %s to use this Item. (%s/%s)",
-	lotteryFail = "You earned nothing from the Lottery.",
-	lotteryProfit = "You earned %s from the Lottery Winning Prize.",
-	["Gun Efficiency"] = "Gun Efficiency",
+	depositMoney = "You have added %s to your account",
+	withdrawMoney = "You have taken %s from your account",
+	hospitalPrice = "Your bank account has been deducted by %s for medical care",
+	provideValidNumber = "You entered the wrong amount",
+	cantAfford = "You can't afford it",
+	enterAmount = "Enter the amount",
+	requireAttrib = "You must continue to train to use this item. (%s: %s/%s)",
+	lotteryFail = "You didn't win anything in the lottery"
+	lotteryProfit = "You won %s in the lottery.",
+	["Gun Efficiency"] = "Shooting Performance",
 	["Medical Knowledge"] = "Medical Knowledge",
-	["Melee Efficiency"] = "Melee Efficiency",
-	["Stamina"] = "Stamina",
-	["Endurance"] = "Endurance",
+	["Melee Efficiency"] = "Close Combat Mastery"
+	["Stamina"] = "Stamina"
+	["Endurance"] = "Endurance"
 
 	-- BAR LANGUAGES
 	stm = "STAMINA",
 	health = "HEALTH",
 	armor = "ARMOR",
-	hunger = "HUNGER",
+	hunger = "FAIM",
+
+	-- ITEM CATEGORY TRANSLATION
+	[ "Ammunition" ] = "Ammunition",
+	["Communication"] = "Communication",
+	["Female Clothes"] = "Female Clothes"
+	["Male Clothes"] = "Male Clothes",
+	["Medical"] = "Medical Materials",
+	["Outfit"] = "Clothes."
+	["Illegal"] = "Illegal Goods",
+	["Consumable"] = "Consumable"
+	["Storage"] = "Storage"
+	["Education"] = "Education",
+
+	-- GROUP PLUGIN TRANSLATION
+	groupCreated = "The %s group has been created",
+	groupDeleted = "The %s group has been disbanded.",
+	groupPermission = "You do not have authorization to execute this command."
+	groupFail = "The creation of the group failed.",
+	groupExists = "This group already exists."
+	groupInvalid = "The organization does not exist."
+	groupHUDLeader = "Responsible for group '%s'.",
+	groupHUD = "Member of group '%s'.",
+	groupGotKicked = "You've been kicked out of the group.",
+	groupNotMember = "The target does not belong to a group.",
+	groupKicked = "You've kicked %s out of the group.",
+	groupShort = "The name of the group is too short.",
+	groupChar = "You're part of the group %s.",
+
+	-- FIRST PERSON PLUGIN TRANSLATION
+	toggleEffects = "Use of first person effects",
+
+	-- RADIO PLUGIN TRANSLATION
+	radioFreq = "Radio frequency",
+	radioSubmit = "Set",
+	RadioNoRadio = "No radio signal."
+	radioNoRadioComm = "No radio communications."
+	radioFormat = "(Radio) %s: \"%s\"",
+
+	-- VEHICLE PLUGIN TRANSLATION
+	vehicleDesc = "You have changed your car information: %s.",
+	vehicleExists = "You already have a vehicle deployed."
+	vehicleStored = "You have stored your vehicle in the garage."
+	notSky = "You need to go outside to call your car.",
+	vehicleSpawned = "Your vehicle has been delivered to you.",
+	vehicleCloser = "You need to get closer of your vehicle.",
+	vehicleStoredDestroyed = "You have stored a damaged vehicle,"
+	vehicleGasFilled = "You have filled up the vehicle with %d%%."
+	vehicleGasLook = "You must look at the vehicle to fill up with fuel."
+
+	-- STASH PLUGIN TRANSLATION\
+	stash = "Stash",
+	stashIn = "Stash in",
+	stashOut = "Stash out",
+	stashError = "Impossible to store things in the stash",
+	stashMenu = "Stash menu",
+	stashDesc = "You can safely store your items here."
+
+	-- ITEM INTERACTION TRANSLATION
+	Unequip = "un-equip",
+	Equip = "Equip",
+	Preview = "Preview",
+	Use = "Use",
+	["Use Forward"] = "Use Forward",
+	Load = "Load",
+	Eat = "Eat",
+	Examine = "Examine",
+	View = "Display",
+	Read = "Read",
+	Drink = "Read",
+	Toggle = "Toggle",
+	Freq = "Frequency",
+	Spawn = "Spawn"
+	Sell = "Shell",
+	Store = "Store",
+
 }
