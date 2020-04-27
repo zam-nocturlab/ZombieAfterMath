4e9fe9cbab1dcccd228aad32e3c72a20b540220e
Blueberry <36592509+Blueberryy@users.noreply.github.com>
2020-04-27 23:13:30 +0500

    Add files via upload (#33)

---
+++ schema/languages/sh_russian.lua
@@ -0,0 +1,106 @@
+LANGUAGE = {
+	
+	reserveSalary = "Вы только что получили письмо от вашего работодателя: Ваша зарплата в размере %s только что была переведена на ваш счёт",
+	reserveIncome = "На ваш банковский щет было только что переведено: %s",
+	salaryRejected = "Вы не можете получить зарплату",
+	bankMenu = "Банк",
+	bankReserve = "Бюджетный резерв: %s",
+	deposit = "Взнос",
+	withdraw = "Забрать",
+	transfer = "Перевод",
+	depositMoney = "Вы добавили %s на ваш счёт",
+	withdrawMoney = "Вы взяли %s с вашего счёта",
+	hospitalPrice = "С вашего банковский счёта было вычтено %s за медицинскую помощь",
+	provideValidNumber = "Вы ввели неправильную сумму",
+	cantAfford = "Вы не можете себе это позволить",
+	enterAmount = "Введите сумму",
+	requireAttrib = "Вы должны продолжать тренироваться, чтобы использовать этот предмет. (%s: %s/%s)",
+	lotteryFail = "Вы ничего не выиграли в лотерее"
+	lotteryProfit = "Вы выиграли %s в лотерее.",
+	["Gun Efficiency"] = "Эффективность стрельбы",
+	["Medical Knowledge"] = "Знание медицины",
+	["Melee Efficiency"] = "Боевое мастерство ближнего боя"
+	["Stamina"] = "Выносливость"
+	["Endurance"] = "Стойкость"
+
+	-- BAR LANGUAGES
+	stm = "ВЫНОСЛИВОСТЬ",
+	health = "ЗДОРОВЬЕ",
+	armor = "БРОНЯ",
+	hunger = "ГОЛОД",
+
+	-- ITEM CATEGORY TRANSLATION
+	[ "Ammunition" ] = "Амуниция",
+	["Communication"] = "Коммуникация",
+	["Female Clothes"] = "Женская одежда"
+	["Male Clothes"] = "Мужская одежда",
+	["Medical"] = "Медицинские материалы",
+	["Outfit"] = "Одежда."
+	["Illegal"] = "Незаконные товары",
+	["Consumable"] = "Потребляемое"
+	["Storage"] = "Хранилище"
+	["Education"] = "Обучение",
+
+	-- GROUP PLUGIN TRANSLATION
+	groupCreated = "Группа %s была создана",
+	groupDeleted = "Группа %s была расформирована.",
+	groupPermission = "У вас нет прав для выполнения этой команды."
+	groupFail = "Создание группы не удалось.",
+	groupExists = "Эта группа уже существует."
+	groupInvalid = "Организация не существует."
+	groupHUDLeader = "Ответственный за группу '%s'.",
+	groupHUD = "Участник группы '%s'.",
+	groupGotKicked = "Вас выгнали из группы.",
+	groupNotMember = "Цель не состоит в группе.",
+	groupKicked = "Вы выгнали %s из группы.",
+	groupShort = "Название группы слишком короткое.",
+	groupChar = "Вы часть группы %s.",
+
+	-- FIRST PERSON PLUGIN TRANSLATION
+	toggleEffects = "Использовать эффекты от первого лица",
+
+	-- RADIO PLUGIN TRANSLATION
+	radioFreq = "Радиочастота",
+	radioSubmit = "Установить",
+	RadioNoRadio = "Нет радиосигнала."
+	radioNoRadioComm = "Нет радиосвязи."
+	radioFormat = "(Радио) %s: \"%s\"",
+
+	-- VEHICLE PLUGIN TRANSLATION
+	vehicleDesc = "Вы изменили информацию об автомобиле: %s.",
+	vehicleExists = "У вас уже есть развёрнутый автомобиль."
+	vehicleStored = "Вы сохранили свой автомобиль в гараже."
+	notSky = "Вы должны выйти на улицу, чтобы вызвать свой автомобиль.",
+	vehicleSpawned = "Ваш автомобиль был доставлен вам.",
+	vehicleCloser = "Вам нужно подойти ближе к вашему автомобилю.",
+	vehicleStoredDestroyed = "Вы сохранили поврежденный автомобиль,"
+	vehicleGasFilled = "Вы заправили автомобиль на %d%%."
+	vehicleGasLook = "Вы должны посмотреть на автомобиль, чтобы заправить его топливом."
+
+	-- STASH PLUGIN TRANSLATION\
+	stash = "Тайник",
+	stashIn = "Положить",
+	stashOut = "Забрать",
+	stashError = "Невозможно хранить вещи в тайнике",
+	stashMenu = "Меню тайника",
+	stashDesc = "Вы можете безопасно хранить свои вещи здесь."
+
+	-- ITEM INTERACTION TRANSLATION
+	Unequip = "Снять",
+	Equip = "Надеть",
+	Preview = "Предпросмотр",
+	Use = "Использ.",
+	["Use Forward"] = "Идти вперед",
+	Load = "Загр.",
+	Eat = "Съесть",
+	Examine = "Исследовать",
+	View = "Показать",
+	Read = "Прочитать",
+	Drink = "Выпить",
+	Toggle = "Переключить",
+	Freq = "Частота",
+	Spawn = "Создать"
+	Sell = "Продать",
+	Store = "Хранить",
+
+}
